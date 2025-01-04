6feafa7c2d6692abc9d687b6698f9b38d521f076
NoahKoh <kohyusheng@gmail.com>
2025-01-05 00:35:49 +0800

    Implemented Locators for Page Objects

---
+++ Selenium/Project/Locators/locators.py
@@ -0,0 +1,10 @@
+class Locators():
+
+    #login page objects
+    username_textbox_name = "username"
+    password_textbox_name = "password"
+    login_button_xpath = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+
+    #home page objects
+    userdropdown_classname = "oxd-userdropdown-tab"
+    logout_button_linktext = "Logout"
--- Selenium/Project/Pages/homePage.py
+++ Selenium/Project/Pages/homePage.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
+from Selenium.Project.Locators.locators import Locators
 
 class HomePage():
 
     def __init__(self, driver):
         self.driver = driver
-        self.userdropdown_classname = "oxd-userdropdown-tab"
-        self.logout_button_linktext = "Logout"
+        self.userdropdown_classname = Locators.userdropdown_classname
+        self.logout_button_linktext = Locators.logout_button_linktext
 
     def click_userdropdown(self):
         userdropdown = self.driver.find_element(By.CLASS_NAME, self.userdropdown_classname)
